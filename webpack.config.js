fd3d439ff7fefa0ccd791f710a2b401f6c04b27b
Arnaud Valle <arnaud.valle@gmail.com>
2016-04-09 17:43:49 +0200

    add basic deploy script and reorganise app with a /build/ folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,37 @@
+var path = require("path");
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// flag passed in to build command
+var production = process.argv.indexOf("--production") > -1;
+
+var PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
 
 module.exports = {
     entry: './app/js/main.js',
     output: {
-        path: './app/',
-        filename: './app/bundled.js'
+        path: PATHS.build,
+        publicPath: '/',
+        filename: 'bundled.js'
     },
     devServer: {
+        contentBase: PATHS.build,
         inline: true, // reload on the fly
         port: 3000
     },
     module: {
+        // note: loaders are loaded in a reverse order, the first one is
+        // actually loaded last, using the result of the previous one
         loaders: [
             {
               test: /\.scss$/,
               loader: ExtractTextPlugin.extract('css!autoprefixer!sass')
             },
             {
+                // all files finishing by .js
                 test: /\.js$/,
+                // but not in the node_modules folder
                 exclude: /node_modules/,
                 loader: 'babel',
                 query: {
@@ -27,7 +41,7 @@ module.exports = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin('app/css/main.css', {
+        new ExtractTextPlugin('main.css', {
             allChunks: true
         })
     ]
